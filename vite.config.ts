de20faadba9bfc24d84870eae979e3f201f56873
Pongaluke <51413507+lukeponga-dev@users.noreply.github.com>
2025-10-18 07:31:30 +1300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     server: {
-      port: 3001,
+      port: 3000,
       host: '0.0.0.0',
       allowedHosts: ['rx8chatbot.onrender.com'], // 👈 Explicitly allow this host
     },
